edef3ffe1d57ed1ffdc0e20ba08a5aacab60c4ec
Huy Nguyen <huy.nn225335@sis.hust.edu.vn>
2025-08-27 10:12:04 +0700

    fix cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -16,7 +16,7 @@ export const AboutSection = () => {
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                             <a href="#contact" className="cosmic-button">Get In Touch</a>
-                            <a href="/public/cv/CV_NguyenNhatHuy.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download CV</a>
+                            <a href="/cv/CV_NguyenNhatHuy.pdf" download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">Download CV</a>
                         </div>
                     </div>
                     <div className="grid grid-cols-1 gap-6">
